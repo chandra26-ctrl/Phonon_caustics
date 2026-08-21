e806280e8525abd100447bb8720f2b10b2db26da
chandra <chandra26@wisc.edu>
2026-08-20 20:27:03 -0500

    added pictures and cleaned up caustics plot

--- Caustics_Plots.C
+++ Caustics_Plots.C
@@ -3,11 +3,12 @@
 
 const int nBinsX = 300;
 const int nBinsY = 300;
-const double minX = -0.004;
-const double minY = -0.004;
-const double maxX = 0.004;
-const double maxY = 0.004;
+const double minX = -4;
+const double minY = -4;
+const double maxX = 4;
+const double maxY = 4;
 
+const double meterToMillimeter = 1000.0;
 
 //---------------------------------------------------------------------------------
 //Only plot transverse fast phonons
@@ -16,7 +17,7 @@ void TransFast(const TString& fileName) {
   //Get the file output from the example
   ifstream in;
   in.open(fileName);
-  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [m];Y position [m]",nBinsX,minX,maxX,nBinsY,minY,maxY);
+  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [mm];Y position [mm]",nBinsX,minX,maxX,nBinsY,minY,maxY);
   Int_t nlines = 0;
   Int_t EventID=-1, TrackID=-1;
   TString Name_Phonon;
@@ -27,7 +28,7 @@ void TransFast(const TString& fileName) {
 
      in>>EventID>>TrackID>>Name_Phonon>>X_f>>Y_f>>Z_f;
      if(Name_Phonon=="phononTF"){
-     Caustics->Fill(X_f,Y_f);
+     Caustics->Fill(X_f * meterToMillimeter,Y_f * meterToMillimeter);
     }
      if (!in.good()) break;
   nlines++;
@@ -53,7 +54,7 @@ void TransSlow(const TString& fileName) {
   //Get the file output from the example
   ifstream in;
   in.open(fileName);
-  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [m];Y position [m]",nBinsX,minX,maxX,nBinsY,minY,maxY);
+  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [mm];Y position [mm]",nBinsX,minX,maxX,nBinsY,minY,maxY);
   Int_t nlines = 0;
   Int_t EventID=-1, TrackID=-1;
   TString Name_Phonon;
@@ -63,7 +64,7 @@ void TransSlow(const TString& fileName) {
   while (1) {    
     in>>EventID>>TrackID>>Name_Phonon>>X_f>>Y_f>>Z_f;
     if(Name_Phonon=="phononTS"){
-      Caustics->Fill(X_f,Y_f);
+      Caustics->Fill(X_f * meterToMillimeter,Y_f * meterToMillimeter);
     }
     if (!in.good()) break;
     nlines++;
@@ -85,14 +86,53 @@ void TransSlow(const TString& fileName) {
 
 }
 
-
 //---------------------------------------------------------------------------------
-void TransFast_and_Slow(const TString& fileName) {
+//Only plot Longitudinal phonons together
+void Longitudinal(const TString& fileName) {
 
   //Get the file output from the example
   ifstream in;
   in.open(fileName);
-  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [m];Y position [m]",nBinsX,minX,maxX,nBinsY,minY,maxY);
+  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [mm];Y position [mm]",nBinsX,minX,maxX,nBinsY,minY,maxY);
+  Int_t nlines = 0;
+  Int_t EventID=-1, TrackID=-1;
+  TString Name_Phonon;
+  Double_t X_f,Y_f,Z_f;
+  
+  //Read in the phonons from the file
+  while (1) {    
+    in>>EventID>>TrackID>>Name_Phonon>>X_f>>Y_f>>Z_f;
+    if(Name_Phonon=="phononL"){
+      Caustics->Fill(X_f * meterToMillimeter,Y_f * meterToMillimeter);
+    }
+    if (!in.good()) break;
+    nlines++;
+  }
+
+  
+  //printf(" found %d points\n",nlines);
+  //gStyle->SetPalette(kBlack+3);
+  TCanvas *c1 = new TCanvas("c1","Canvas Example",200,10,600,480);
+  c1->SetLeftMargin(0.15);  // Leave room for the Y-axis title
+  c1->SetFillColor(kWhite);
+  //Caustics->SetMarkerColorAlpha(kWhite, 0.2);
+  c1->SetLogz();
+
+  Caustics->SetMinimum(1);
+  //Caustics->Draw("colz"); // If you want to see on a Blue and yellow colors
+  Caustics->Draw("colz");
+  in.close();
+
+}
+
+//---------------------------------------------------------------------------------
+// plot all phonons together
+void all(const TString& fileName) {
+
+  //Get the file output from the example
+  ifstream in;
+  in.open(fileName);
+  TH2D *Caustics= new TH2D("Caustics","Phonon Caustics;X position [mm];Y position [mm]",nBinsX,minX,maxX,nBinsY,minY,maxY);
   Int_t nlines = 0;
   Int_t EventID=-1, TrackID=-1;
   TString Name_Phonon;
@@ -103,7 +143,7 @@ void TransFast_and_Slow(const TString& fileName) {
 
      in>>EventID>>TrackID>>Name_Phonon>>X_f>>Y_f>>Z_f;
 
-     Caustics->Fill(X_f,Y_f);
+     Caustics->Fill(X_f * meterToMillimeter,Y_f * meterToMillimeter);
 
      if (!in.good()) break;
   nlines++;
@@ -130,18 +170,22 @@ using namespace std;
 void Caustics_Plots(TString Phonon_Name, TString fileName="phonon_hits.txt") {
   
   TString  Phonon_Case = Phonon_Name;
-  Int_t What_phonon1,What_phonon2,What_phonon3;
+  Int_t What_phonon1,What_phonon2,What_phonon3,What_phonon4;
   What_phonon1=Phonon_Case.CompareTo("Fast");
   What_phonon2=Phonon_Case.CompareTo("Slow");
-  What_phonon3=Phonon_Case.CompareTo("Both");
+  What_phonon3=Phonon_Case.CompareTo("Long");
+  What_phonon4=Phonon_Case.CompareTo("All");
   if (What_phonon1==0) {
     cout<<"Fast";
     TransFast(fileName);
   } else if (What_phonon2==0) {
     cout<<"Slow";
     TransSlow(fileName);
-  } else {
-    cout<<"Both";
-    TransFast_and_Slow(fileName);
+  } else if (What_phonon3==0) {
+    cout<<"Long";
+    Longitudinal(fileName);
+  } else if (What_phonon4==0) {
+    cout<<"All";
+    all(fileName);
   }
 }
--- src/Caustic_PhononPrimaryGeneratorAction.cc
+++ src/Caustic_PhononPrimaryGeneratorAction.cc
@@ -40,17 +40,20 @@ void Caustic_PhononPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
   // This is only to distinguish the Phonons Caustics
   // Note.- This is not the initial density of States of the Substrate
   G4double selector = G4UniformRand();
-  if (selector<0.5) {
-  //  fParticleGun->SetParticleDefinition(G4PhononTransFast::Definition());
+  if (selector<0.33) {
+    fParticleGun->SetParticleDefinition(G4PhononTransFast::Definition());
 
   }
-  else {
-  //  fParticleGun->SetParticleDefinition(G4PhononTransSlow::Definition());
+  else if (selector<0.66) {
+    fParticleGun->SetParticleDefinition(G4PhononTransSlow::Definition());
   // fParticleGun->SetParticleDefinition(G4PhononLong::Definition());
   // If you are interested in Longitudinal phonons.
   // You only need to uncomment and comment on the other fParticleGun.
   }
+  else {
     fParticleGun->SetParticleDefinition(G4PhononLong::Definition());
+  }
+  //  fParticleGun->SetParticleDefinition(G4PhononLong::Definition());
      fParticleGun->GeneratePrimaryVertex(anEvent);
 }
 
